5c85abb2b6e0e968f6a7ca4745582083118a24bd
Robert Nowotny <bitranox@users.noreply.github.com>
2019-04-03 21:42:36 +0200

    improove code coverage

--- wine_setup_32bit.sh
+++ wine_setup_32bit.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 # Setup Wine 32 Bit
+mkdir ~/wine
 WINEARCH=win32 WINEPREFIX=~/wine/wine32 winecfg
--- wine_setup_64bit.sh
+++ wine_setup_64bit.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # Setup Wine 64 Bit
+mkdir ~/wine
 WINEPREFIX=~/wine/wine64 winecfg
-
