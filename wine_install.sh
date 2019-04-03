e5d595b3a3636ab42176a8af07f75a8bc8f2da4f
Robert Nowotny <bitranox@users.noreply.github.com>
2019-04-03 21:08:54 +0200

    improove code coverage

--- wine_install.sh
+++ wine_install.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 echo "Build Start"
 sudo dpkg --add-architecture i386
-git clone git://source.winehq.org/git/wine.git
-cd wine
+REM git clone git://source.winehq.org/git/wine.git
+REM cd wine
+REM sudo apt-get update
+REM sudo apt-get install build-essential
+REM ./configure
+REM ./make
+REM cd ..
+
+wget -nc https://dl.winehq.org/wine-builds/winehq.key
+sudo apt-key add winehq.key
+sudo apt-add-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ xenial main'
 sudo apt-get update
-sudo apt-get install build-essential
-./configure
-./make
-cd ..
+sudo apt-get install --install-recommends winehq-devel
