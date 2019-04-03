f5d6f65761b4d2b1d9129e210b61961d2f18650f
Robert Nowotny <bitranox@users.noreply.github.com>
2019-04-04 00:30:26 +0200

    upload compiled python3.7.3 32/64 Bit for wine

--- wine_setup_32bit.sh
+++ wine_setup_32bit.sh
@@ -6,5 +6,15 @@ echo "Setup Wine 32 Bit"
 mkdir -p ~/wine
 DISPLAY=:0 WINEARCH=win32 WINEPREFIX=~/wine/wine32 winecfg
 
-cd ${save_path}
 
+echo "Download Python 3.7.3 32 Bit"
+cd ~
+wget --no-check-certificate --content-disposition https://github.com/bitranox/python_wine_binaries/bin/python3.7.3_wine_32.zip
+
+echo "Unzip Python 3.7.3 32 Bit"
+unzip python3.7.3_wine_32.zip -d ~/wine/wine32/drive_c
+
+cd ~/wine/wine32/drive_c
+ls -l
+
+cd ${save_path}
