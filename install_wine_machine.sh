10cd647578e7b3ac2bffffa4996791abf1962604
Robert Nowotny <bitranox@users.noreply.github.com>
2019-04-04 19:08:20 +0200

    upload compiled python3.7.3 32/64 Bit for wine

--- install_wine_machine.sh
+++ install_wine_machine.sh
@@ -5,14 +5,12 @@ echo "Setup Wine Machine at ${WINEPREFIX}, WINEARCH=${WINEARCH} "
 mkdir -p ${WINEPREFIX}
 wine_drive_c_dir=${WINEPREFIX}/drive_c
 
-Xvfb :99 -screen 0 1024x768x24 > /dev/null 2>&1 &
-winecfg
+xvfb-run --auto-servernum winecfg
 
 echo "Disable GUI Crash Dialogs"
 winetricks nocrashdialog
 
 echo "Install common Packets"
-# winetricks -q msxml3  # win32 ok
 winetricks -q msxml6
 winetricks -q dotnet462
 winetricks -q allfonts
