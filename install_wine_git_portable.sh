123c2b854467664658fe69b12fc72797966e26d7
Robert Nowotny <bitranox@users.noreply.github.com>
2019-04-04 22:40:49 +0200

    try to install wine

--- install_wine_git_portable.sh
+++ install_wine_git_portable.sh
@@ -2,20 +2,18 @@
 save_path="`dirname \"$0\"`"
 
 wine_drive_c_dir=${WINEPREFIX}/drive_c
-git_install_dir=${wine_drive_c_dir}/install/git_portable
-mkdir -p ${git_install_dir}
 
-cd ${git_install_dir}
+cd ${HOME}
 
-wget --no-check-certificate -O pywine-master.zip https://github.com/bitranox/python_wine_binaries/archive/master.zip
+wget -nc --no-check-certificate -O pywine-master.zip https://github.com/bitranox/python_wine_binaries/archive/master.zip
 
 if [[ "${WINEARCH}" == "win32" ]]
     then
         echo "Unzip Git Portable 32 Bit"
-        unzip -qq ${git_install_dir}/python_wine_binaries-master/bin/PortableGit32_2.21.0.zip -d ${wine_drive_c_dir}
+        unzip -qq ${HOME}/python_wine_binaries-master/bin/PortableGit32_2.21.0.zip -d ${wine_drive_c_dir}
     else
         echo "Unzip Git Portable 64 Bit"
-        unzip -qq ${git_install_dir}/python_wine_binaries-master/bin/PortableGit64_2.21.0.zip -d ${wine_drive_c_dir}
+        unzip -qq ${HOME}/python_wine_binaries-master/bin/PortableGit64_2.21.0.zip -d ${wine_drive_c_dir}
     fi
 
 cd ${save_path}
