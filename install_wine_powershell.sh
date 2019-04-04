efc4b84767df0b96514d6fe56d43c9f9d37bcf96
Robert Nowotny <bitranox@users.noreply.github.com>
2019-04-04 18:18:15 +0200

    upload compiled python3.7.3 32/64 Bit for wine

--- install_wine_powershell.sh
+++ install_wine_powershell.sh
@@ -10,17 +10,12 @@ cd ${powershell_install_dir}
 if [[ "${WINEARCH}" == "win32" ]]
     then
         echo "Download Powershell 32 Bit"
-        # wget --no-check-certificate -O powershell.zip https://github.com/PowerShell/PowerShell/releases/download/v6.2.0/PowerShell-6.2.0-win-x86.zip
         wget --no-check-certificate -O powershell.msi https://github.com/PowerShell/PowerShell/releases/download/v6.2.0/PowerShell-6.2.0-win-x86.msi
     else
         echo "Download Powershell 64 Bit"
-        # wget --no-check-certificate -O powershell.zip https://github.com/PowerShell/PowerShell/releases/download/v6.2.0/PowerShell-6.2.0-win-x64.zip
         wget --no-check-certificate -O powershell.msi https://github.com/PowerShell/PowerShell/releases/download/v6.2.0/PowerShell-6.2.0-win-x64.msi
     fi
 
-# unzip -qq ./powershell.zip -d ${powershell_dir}
-# wine ${powershell_dir}/pwsh -h
-
-wine msiexec /i ${powershell_install_dir}/powershell.msi /quiet
+wine msiexec /i ${powershell_install_dir}/powershell.msi
 
 cd ${save_path}
