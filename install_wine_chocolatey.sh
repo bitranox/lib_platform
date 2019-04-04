5141cfbe88a29cc5931407712335051d4821bfa1
Robert Nowotny <bitranox@users.noreply.github.com>
2019-04-04 19:37:30 +0200

    upload compiled python3.7.3 32/64 Bit for wine

--- install_wine_chocolatey.sh
+++ install_wine_chocolatey.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 save_path="`dirname \"$0\"`"
 
-wine c:/windows/system32/powershell/pwsh.exe -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+wine ${powershell_install_dir}/pwsh -ExecutionPolicy unrestricted get-executionpolicy
+
+wine c:/windows/system32/powershell/pwsh.exe -NoProfile -InputFormat None -ExecutionPolicy unrestricted -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 
 cd ${save_path}
