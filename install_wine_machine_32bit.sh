aa6add82b8de0874e42c96876bce25188c2de63b
Robert Nowotny <bitranox@users.noreply.github.com>
2019-04-04 13:01:08 +0200

    upload compiled python3.7.3 32/64 Bit for wine

--- install_wine_machine_32bit.sh
+++ install_wine_machine_32bit.sh
@@ -3,32 +3,28 @@ save_path="`dirname \"$0\"`"
 
 ##########################################
 echo "Setup Wine 32 Bit"
-# mkdir -p ~/wine
 mkdir -p ${HOME}/wine
-# wine_dir="~/wine/wine32"
-wine_dir="${HOME}/wine/wine32"
-# eval wine_dir=${wine_dir}  ## expand the home dir - wine does not like "~"
+wine_dir="${HOME}/wine/wine32"   ## expand the home dir with ${HOME} - wine does not like "~"
+wine_drive_c_dir=${wine_dir}/drive_c
 
 export WINEARCH=win32
 export WINEPREFIX=${wine_dir}
 WINEARCH=win32 WINEPREFIX=${wine_dir} winecfg
 
-##########################################
 echo "Disable GUI Crash Dialogs"
 xvfb-run winetricks nocrashdialog
 
-##########################################
-echo "Download Python 3.7.3 32 Bit"
+echo "Download Python Binaries"
 cd ~
 wget --no-check-certificate -O pywine32.zip https://github.com/bitranox/python_wine_binaries/archive/master.zip
 # NOT WORKING :
 # wget --no-check-certificate -O pywine32.zip https://github.com/bitranox/python_wine_binaries/blob/master/bin/python3.7.3_wine_32.zip # not working !
 
-##########################################
-wine_drive_c_dir=${wine_dir}/drive_c
+
 echo "Unzip Python 3.7.3 32 Bit to ${wine_drive_c_dir}"
-unzip ./pywine32.zip -d ${wine_drive_c_dir}
-cd ${wine_drive_c_dir}
+unzip ./pywine-master.zip -d ${HOME}
+unzip ./python_wine_binaries-master/bin/python3.7.3_wine_32.zip -d ${wine_drive_c_dir}
+
 echo "this is now the content of ${wine_drive_c_dir}"
 ls -l
 
