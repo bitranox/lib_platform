0b90ec5168bb47c07a61b4bd7bf1778d242f2fa6
Robert Nowotny <bitranox@users.noreply.github.com>
2019-04-05 10:23:07 +0200

    try to install wine

--- install_wine_git_portable.sh
+++ install_wine_git_portable.sh
@@ -2,21 +2,21 @@
 save_path="`dirname \"$0\"`"
 
 wine_drive_c_dir=${WINEPREFIX}/drive_c
+decompress_dir=${HOME}/decompress
 
 echo "Download Git Portable Binaries"
-cd ${HOME}
-wget -nc --no-check-certificate -O binaries_portable_git-master.zip https://github.com/bitranox/binaries_portable_git/archive/master.zip
+wget -nc --no-check-certificate -O ${decompress_dir}/binaries_portable_git-master.zip https://github.com/bitranox/binaries_portable_git/archive/master.zip
 
 echo "Unzip Git Portable Binaries Master to ${HOME}"
-unzip -nqq ./binaries_portable_git-master.zip -d ${HOME}
+unzip -nqq ${decompress_dir}/binaries_portable_git-master.zip -d ${decompress_dir}
 
 if [[ "${WINEARCH}" == "win32" ]]
     then
-        echo "Unzip Git Portable Binaries 32 Bit to ${wine_drive_c_dir}"
-        unzip -qq ./binaries_portable_git-master/bin/PortableGit32_v2.21.0.zip -d ${wine_drive_c_dir}
+        echo "Unzip Git Portable Binaries 32 Bit to ${decompress_dir}"
+        unzip -qq ${decompress_dir}/binaries_portable_git-master/bin/PortableGit32*.zip -d ${wine_drive_c_dir}
     else
-        echo "Unzip Git Portable Binaries 64 Bit to ${wine_drive_c_dir}"
-        unzip -qq ./binaries_python_wine-master/bin/PortableGit64_v2.21.0.zip -d ${wine_drive_c_dir}
+        echo "Unzip Git Portable Binaries 64 Bit to ${decompress_dir}"
+        unzip -qq ${decompress_dir}/binaries_portable_git-master/bin/PortableGit64*.zip -d ${wine_drive_c_dir}
     fi
 
 cd ${save_path}
--- install_wine_python_preinstalled.sh
+++ install_wine_python_preinstalled.sh
@@ -2,21 +2,21 @@
 save_path="`dirname \"$0\"`"
 
 wine_drive_c_dir=${WINEPREFIX}/drive_c
+decompress_dir=${HOME}/decompress
 
 echo "Download Python Binaries"
-cd ${HOME}
-wget -nc --no-check-certificate -O pywine-master.zip https://github.com/bitranox/binaries_python_wine/archive/master.zip
+wget -nc --no-check-certificate -O ${decompress_dir}/binaries_python_wine-master.zip https://github.com/bitranox/binaries_python_wine/archive/master.zip
 
 echo "Unzip Python 3.7.3 Master to ${HOME}"
-unzip -nqq ./pywine-master.zip -d ${HOME}
+unzip -nqq ${decompress_dir}/binaries_python_wine-master.zip -d ${decompress_dir}
 
 if [[ "${WINEARCH}" == "win32" ]]
     then
         echo "Unzip Python 3.7.3 32 Bit to ${wine_drive_c_dir}"
-        unzip -qq ./binaries_python_wine-master/bin/python3.7.3_wine_32.zip -d ${wine_drive_c_dir}
+        unzip -qq ${decompress_dir}/binaries_python_wine-master/bin/python3.7.3_wine_32.zip -d ${wine_drive_c_dir}
     else
         echo "Unzip Python 3.7.3 64 Bit to ${wine_drive_c_dir}"
-        unzip -qq ./binaries_python_wine-master/bin/python3.7.3_wine_64.zip -d ${wine_drive_c_dir}
+        unzip -qq ${decompress_dir}/binaries_python_wine-master/bin/python3.7.3_wine_64.zip -d ${wine_drive_c_dir}
     fi
 
 cd ${save_path}
