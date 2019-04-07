52a6f12a38d83172fc89c7def25964f54d99566b
Robert Nowotny <bitranox@users.noreply.github.com>
2019-04-07 22:55:22 +0200

    update __init__.py for local package development

--- __init__.py
+++ __init__.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
+import logging
 
-# this is only for local development when the package is actually not installed
 try:
     from .lib_platform.lib_platform import *
 except ImportError:
-    pass
+    logger = logging.getLogger()
+    logger.debug('Import Error - this __init__.py is only meant for local package development')
 
 __title__ = 'lib_platform'
 __version__ = '1.0.0'
