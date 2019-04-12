bf8df2ac17faa1af4b32f9fc849b9ad89dc65184
Robert Nowotny <bitranox@users.noreply.github.com>
2019-04-12 15:14:20 +0200

    wine installer - create permanent path to python version

--- install_wine_git_portable.sh
+++ install_wine_git_portable.sh
@@ -22,13 +22,21 @@ if [[ "${WINEARCH}" == "win32" ]]
     then
         echo "Joining Multipart Zip in ${decompress_dir}/binaries_portable_git-master/bin"
         cat ${decompress_dir}/binaries_portable_git-master/bin/PortableGit32* > ${decompress_dir}/binaries_portable_git-master/bin/joined_PortableGit.zip
+        add_git_path="c:/PortableGit32/cmd"
     else
         echo "Joining Multipart Zip in ${decompress_dir}/binaries_portable_git-master/bin"
         cat ${decompress_dir}/binaries_portable_git-master/bin/PortableGit64* > ${decompress_dir}/binaries_portable_git-master/bin/joined_PortableGit.zip
+        add_git_path="c:/PortableGit64/cmd"
     fi
 
 echo "Unzip Git Portable Binaries to ${wine_drive_c_dir}"
 unzip -qq ${decompress_dir}/binaries_portable_git-master/bin/joined_PortableGit.zip -d ${wine_drive_c_dir}
+
+echo "add Path Settings to Registry"
+wine_current_reg_path="`wine reg QUERY \"HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Session Manager\Environment\" /v PATH | grep REG_SZ | sed 's/^.*REG_SZ\s*//'`"
+wine_new_reg_path="${add_git_path};${wine_current_reg_path}"
+wine reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Session Manager\Environment" /t REG_SZ /v PATH /d "${wine_new_reg_path}" /f
+
 
 rm -r ${decompress_dir}
 
