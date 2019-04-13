359f244f08280ddf3d961b4688d6af18a526dd04
Robert Nowotny <bitranox@users.noreply.github.com>
2019-04-14 01:19:37 +0200

    wine install refractor

--- lib_bash/lib_retry.sh
+++ lib_bash/lib_retry.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-function include_dependencies{
+function include_dependencies {
     local my_dir="$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )"  # this gives the full path, even for sourced scripts
-    source ${my_dir}/lib_color.sh
+    source "${my_dir}/lib_color.sh"
 }
 
-include_dependencies
+include_dependencies  # me need to do that via a function to have local scope of my_dir
+
 
 function fail {
   clr_bold clr_red "${1}" >&2
--- lib_bash/lib_wine_install.sh
+++ lib_bash/lib_wine_install.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
-function include_dependencies{
+function include_dependencies {
     local my_dir="$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )"  # this gives the full path, even for sourced scripts
-    chmod +x ${my_dir}/*.sh
-    source ${my_dir}/lib_color.sh
-    source ${my_dir}/lib_retry.sh
+    chmod +x "${my_dir}/*.sh"
+    source "${my_dir}/lib_color.sh"
+    source "${my_dir}/lib_retry.sh"
 }
 
-include_dependencies
-
-
+include_dependencies  # me need to do that via a function to have local scope of my_dir
 
 
 function fail {
