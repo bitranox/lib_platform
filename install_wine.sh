cff651ab35c69b501b94e3fbc714d34dfbb0d519
Robert Nowotny <bitranox@users.noreply.github.com>
2019-04-14 21:53:55 +0200

    wine install refractor

--- install_wine.sh
+++ install_wine.sh
@@ -11,8 +11,10 @@ function include_dependencies {
 
 include_dependencies  # me need to do that via a function to have local scope of my_dir
 
-get_linux_codename
-clr_bold clr_green "Install WINE on ${linux_codename}"
+
+
+
+clr_bold clr_green "Install WINE"
 check_wine_version
 
 clr_green "add 386 Architecture"
@@ -20,8 +22,7 @@ retry sudo dpkg --add-architecture i386
 clr_green "add Wine Keys"
 retry wget https://dl.winehq.org/wine-builds/winehq.key
 sudo apt-key add winehq.key
-repository="deb https://dl.winehq.org/wine-builds/ubuntu/ \"${linux_codename}\" main"
-sudo apt-add-repository $repository
+sudo apt-add-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ xenial main'
 clr_green "Wine Packages Update"
 retry sudo apt-get update
 clr_green "Wine Packages Install"
