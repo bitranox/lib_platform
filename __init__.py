4cb74963f268eb4d97d1832acfcc267811792f06
bitranox <rnowotny1966@gmail.com>
2019-08-26 18:22:58 +0200

    refractor

--- __init__.py
+++ __init__.py
@@ -1,8 +1,3 @@
 # -*- coding: utf-8 -*-
-
 # this __init__.py is only meant for local package development
 from .lib_platform import *
-
-__title__ = 'lib_platform'
-__version__ = '1.0.0'
-__name__ = 'lib_platform'
