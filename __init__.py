81b39270dd7e449880c8df48e036058f9130eb96
bitranox <rnowotny1966@gmail.com>
2020-05-21 00:52:07 +0200

    refractor

--- __init__.py
+++
@@ -1,6 +0,0 @@
-# this __init__.py is only meant for local package development
-try:
-    from .lib_platform import *
-# this we need for pip install --install-option test
-except ImportError:
-    import lib_platform
--- lib_platform/__init__.py
+++ lib_platform/__init__.py
@@ -2,23 +2,7 @@
 import os
 
 # PROJECT
-from .lib_platform import system
-from .lib_platform import is_platform_linux
-from .lib_platform import is_platform_darwin
-from .lib_platform import is_platform_posix
-from .lib_platform import is_platform_windows
-from .lib_platform import is_platform_windows_xp
-from .lib_platform import is_platform_windows_wine
-from .lib_platform import is_platform_windows_wine_xp
-from .lib_platform import username
-from .lib_platform import hostname
-from .lib_platform import hostname_short
-from .lib_platform import is_python2
-from .lib_platform import is_python3
-from .lib_platform import path_userhome
-from .lib_platform import get_system                        # this we need for pytest
-from .lib_platform import get_is_platform_windows_xp        # this we need for pytest
-from .lib_platform import is_user_admin
+from .lib_platform import *
 
 
 # Python 2.7 Version
