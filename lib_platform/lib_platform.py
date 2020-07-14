6487c8370c78119dca03b7fb5c3e1ef08f387339
bitranox <rnowotny1966@gmail.com>
2020-07-14 14:34:47 +0200

    refractor

--- lib_platform/lib_platform.py
+++ lib_platform/lib_platform.py
@@ -175,7 +175,8 @@ def get_is_user_admin() -> bool:
         # type ignore is needed here, because does not exist on linux
         _is_user_admin = ctypes.windll.shell32.IsUserAnAdmin() == 1   # type: ignore
     else:
-        _is_user_admin = os.getuid() == 0
+        # type ignore is needed here, because does not exist on windows
+        _is_user_admin = os.getuid() == 0                             # type: ignore
 
     return bool(_is_user_admin)
 
